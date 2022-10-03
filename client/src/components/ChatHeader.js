abd86016b32c9b6c7e7e8e382de59ec2cc3f54ab
Aditya Kumar <adityagamers786@gmail.com>
2022-10-03 15:21:05 +0530

    Initialised with template code

---
+++ client/src/components/ChatHeader.js
@@ -0,0 +1,28 @@
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
+
+const ChatHeader = ({ user }) => {
+    const [ cookies, setCookie, removeCookie ] = useCookies(['user'])
+    let navigate = useNavigate()
+
+
+    const logout = () => {
+        removeCookie('UserId', cookies.UserId)
+        removeCookie('AuthToken', cookies.AuthToken)
+        navigate('/')
+    }
+
+    return (
+        <div className="chat-container-header">
+            <div className="profile">
+                <div className="img-container">
+                    <img src={user.url} alt={"photo of " + user.first_name}/>
+                </div>
+                <h3>{user.first_name}</h3>
+            </div>
+            <i className="log-out-icon" onClick={logout}>⇦</i>
+        </div>
+    )
+}
+
+export default ChatHeader
