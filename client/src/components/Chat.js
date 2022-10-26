00e0610fb57513c271a4b40e2b009192ede8fd73
Aditya Kumar <adityagamers786@gmail.com>
2022-10-26 23:24:03 +0530

    Add refresh chat button temproarily

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,9 @@
-import ScrollButton from './ScrollButton'; 
-const Chat = ({formattedMessages}) => {
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+const Chat = ({getUsersMessages, formattedMessages}) => {
     return (
-        <div>
+        <React.Fragment>
             <div className="chat-display">
                 {formattedMessages.map((message, _index) => (
                     <div key={_index}>
@@ -14,8 +16,11 @@ const Chat = ({formattedMessages}) => {
                         <p>{message.message}</p>
                     </div>
                 ))}
+              <div className='refresh'>
+                <button className="refresh-button" onClick={() => getUsersMessages()}>Refresh Chat</button>
+              </div>
             </div>
-        </div>
+        </React.Fragment>
     )
 }
 
